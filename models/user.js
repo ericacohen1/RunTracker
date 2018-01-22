46285d45c8f0f561c2f012818f02d22b3f8ff68f
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-01-22 13:39:37 -0800

    app talking w/ db

--- models/user.js
+++ models/user.js
@@ -3,26 +3,26 @@ module.exports = function (sequelize, DataTypes) {
     // Define the User model
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     email: {
       // - LC - would like to propose that we change this to "email". Reason being that there is an auto-generated "id" column
       // having another column like "userID" can be confusing. I'm thinking email might be a way we can have unique accounts
       // and allow users to log in with their email address. 
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     age: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     sex: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     password: {
-      type: Datatype.STRING,
-      allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false
     }
   });
 
