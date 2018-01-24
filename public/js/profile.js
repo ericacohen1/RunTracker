12fe747a1d44cad46804537fbcc9886be766e60a
Levi Crouch <levi.crouch@gmail.com>
2018-01-24 15:03:32 -0800

    Toggling the add\edit activity div functional. Updating a record works as well. Need to create a few functions to get the state of buttons and show\hide them

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
     /* global moment */
+    // set the property data-visible to hidden
+    $(".new-activity").attr("data-visible", "hidden");
+    // hide the new activity
+    $(".new-activity").hide()
+
+    toggleNewActivity();
+    var activityId;
+    var updateActivity;
     var url = window.location.search;
     var userId;
     if (url.indexOf("?UserId=") !== -1) {
@@ -8,7 +16,7 @@ $(document).ready(function () {
     }
 
     $(function () {
-        $("#new-workout").on("click", function (e) {
+        $(".add-activity").on("click", function (e) {
             e.preventDefault();
             var newActivityObj = {
                 date: $("#activityDate").val().trim(),
@@ -35,6 +43,9 @@ $(document).ready(function () {
     // Click events for the edit and delete buttons
     $(document).on("click", "button.delete", handleActivityDelete);
     $(document).on("click", "button.edit", handleActivityEdit);
+    $(document).on("click", "button.toggle-activity", handleNewActivity);
+    $(document).on("click", "button.cancel-activity", cancelNewActivity);
+    $(document).on("click", "button.update-activity", updateActivity);
 
     // Variable to hold our activities
     var activities;
@@ -109,8 +120,8 @@ $(document).ready(function () {
             $("<td>").append(activity.totalActivityTime),
             $("<td>").append(activity.pace),
             $("<td>").append("<button class='edit btn btn-primary' data-activity-id='" + activity.id + "' aria-hidden='true'>Edit</button>"),
-            $("<td>").append("<button class='btn btn-primary' data-action='delete' aria-hidden='true'>Delete</button>")
-        )
+            $("<td>").append("<button class='delete btn btn-danger' data-action='delete' aria-hidden='true'>Delete</button>")
+            )
         );
     }
 
@@ -123,19 +134,141 @@ $(document).ready(function () {
         deletePost(currentPost.id);
     }
 
-    // This function figures out which activity we want to edit and takes it to the appropriate url
-    function handleActivityEdit() {
-        var activityId = $(this).attr("data-activity-id");
-        console.log("this.attr('data-activity-id)'",$(this).attr("data-activity-id"));
-        // var currentActivity = $(this)
-        //     .parent()
-        //     .parent()
-        //     .data("data-activity-id");
-        //     console.log("currentActivity",currentActivity);
-            // .attr("data-activity-id");
-        window.location.href = "/activity?activity_id=" + activityId;
+    // function to toggle the new\edit activity div
+    function toggleNewActivity() {
+        var currentState = $(".new-activity").attr("data-visible");
+        console.log("currentState", currentState);
+    }
+    function handleNewActivity() {
+        console.log("in handleNewActivity");
+        // empty existing data
+
+        var currentState = $(".new-activity").attr("data-visible");
+        console.log("currentState", currentState);
+        if (currentState === "hidden") {
+            // set the property data-visible to hidden
+            $(".new-activity").attr("data-visible", "visible");
+            currentState = $(".new-activity").attr("data-visible");
+            console.log("currentState", currentState);
+            // show the new activity div
+            $(".new-activity").show();
+        }
+        // get current state of submit and update button
+        var submitBtnState = $(".add-activity").attr("data-visible");
+        var updateBtnState = $(".update-activity").attr("data-visible");
+        // if submit button is not-set or hidden show it
+        if (submitBtnState === "not-set" || submitBtnState === "hidden") {
+            // show the submit button
+            $(".add-activity").attr("data-visible", "visible");
+            $(".add-activity").show()
+            // hide the update button
+            $(".update-activity").attr("data-visible", "hidden");
+            $(".update-activity").hide()
+        }
+
     }
 
+    // This function figures out which activity we want to edit and takes it to the appropriate url
+    function handleActivityEdit() {
+        console.log("in handleActivityEdit");
+        var currentState = $(".new-activity").attr("data-visible");
+        console.log("currentState", currentState);
+        if (currentState === "hidden") {
+            // set the property data-visible to hidden
+            $(".new-activity").attr("data-visible", "visible");
+            currentState = $(".new-activity").attr("data-visible");
+            console.log("currentState", currentState);
+            // hide the new activity
+            $(".new-activity").show();
+
+        }
+        activityId = $(this).attr("data-activity-id");
+        console.log("this.attr('data-activity-id)'", $(this).attr("data-activity-id"));
+        // convert to integer
+        activityId = parseInt(activityId);
+        // loop through activities array to find the index of the activity we want to edit
+        for (var i = 0; i < activities.length; i++) {
+            var editIndex;
+            var currentId = activities[i].id;
+
+            if (currentId === activityId) {
+                editIndex = i;
+                console.log("Index of activity id:", editIndex);
+            }
+        }
+        // populate the edit fields with the activity data
+        var momentDate = moment(activities[editIndex].date).format("MM/DD/YYYY");
+        console.log("activities[editIndex].date", activities[editIndex].date);
+        $("#activityDate").val(momentDate);
+        $("#totalDistance").val(activities[editIndex].distance);
+        $("#totalRunTime").val(activities[editIndex].totalActivityTime);
+        // toggle to see the update button and hide the submit button
+        // get the current state of the buttons
+        var submitBtnState = $(".add-activity").attr("data-visible");
+        var updateBtnState = $(".update-activity").attr("data-visible");
+        console.log("submitBtnState", submitBtnState);
+        console.log("updateBtnState", updateBtnState);
+        // if submit is not-set or visible hide it with jQuery
+        if (submitBtnState === "not-set" || submitBtnState === "visible") {
+            $(".add-activity").attr("data-visible", "hidden");
+            $(".add-activity").hide();
+        }
+        // if update button is not-set or hidden show it with jQuery
+        if (updateBtnState === "not-set" || updateBtnState === "hidden") {
+            $(".update-activity").attr("data-visible", "visible");
+            $(".update-activity").show();
+        }
+        // updatedActivity = {
+        //     id: activityId,
+        //     date: $("#activityDate").val().trim(),
+        //     distance: $("#totalDistance").val().trim(),
+        //     totalActivityTime: $("#totalRunTime").val().trim(),
+        //     UserId: userId,
+        //     pace: 7
+        // }
+
+    }
+    function updateActivity() {
+        // activityId = id;
+        // path to update a record: /api/activity/:id
+        updatedActivity = {
+            id: activityId,
+            date: $("#activityDate").val().trim(),
+            distance: $("#totalDistance").val().trim(),
+            totalActivityTime: $("#totalRunTime").val().trim(),
+            UserId: userId,
+            pace: 7
+        }
+        $.ajax({
+            method: "PUT",
+            url: "/api/activity/" + activityId,
+            data: updatedActivity
+        }).then(function () {
+            // location.reload();
+        });
+    }
+    function clearFields() {
+        // clear the new\edit fields
+        $("#activityDate").val("");
+        $("#totalDistance").val("");
+        $("#totalRunTime").val("");
+    }
+
+    function cancelNewActivity() {
+        // cancels an edit or new activity
+        console.log("in cancelNewActivity");
+        var currentState = $(".new-activity").attr("data-visible");
+        console.log("currentState", currentState);
+        if (currentState === "visible") {
+            // set the property data-visible to hidden
+            $(".new-activity").attr("data-visible", "hidden");
+            currentState = $(".new-activity").attr("data-visible");
+            console.log("currentState", currentState);
+            // hide the new activity
+            $(".new-activity").hide();
+            clearFields();
+        }
+    }
     // This function displays a messgae when there are no posts
     function displayEmpty(id) {
         var query = window.location.search;
